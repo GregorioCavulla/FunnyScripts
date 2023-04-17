433823bb7da4f49f0f38ca44765a53184569d936
GregorioCavulla::ghigo <ghigo2000.mr@gmail.com>
2023-04-17 18:23:18 +0200

    Update TomatoTimer.bat
    Minor bug fixes.
    Added local counter to keep track of loops.
    Added output to a log file.

--- TomatoTimer.bat
+++ TomatoTimer.bat
@@ -1,11 +1,21 @@
 @echo off
+setlocal enabledelayedexpansion
+set count=0
+set cc=1
+
+set output_f=C:\your\output\file.txt
+echo !date! !time! >> "%output_f%"
+
 :loop
-echo Timer iniziato
+echo Timer !cc! iniziato
 timeout /t 750 /nobreak >nul
-echo Sei a metà non mollare
+echo Non mollare!
 timeout /t 750 /nobreak >nul
 echo Sono passati 25 minuti
 timeout /t 300 /nobreak >nul
 echo Sono passati 5 minuti
+set /a count+=1
+set /a cc+=1
+echo Loop numero: !count! >> "%output_f%"
 pause
 goto loop
